2c26c9eef6e6cdf61efe1a2bdde88d9b7a8aa351
Aditya choudhary <adityachoudhary54@gmail.com>
2018-07-25 00:51:35 +0530

    added bottom up approach of fibonacci DP

--- C++ important files/Dynamic programming/fibbyDP.cpp
+++ C++ important files/Dynamic programming/fibbyDP.cpp
@@ -20,6 +20,33 @@ long long int topdowndp(int n,long long int dp[])
 		dp[n]=topdowndp(n-1,dp)+topdowndp(n-2,dp);
 		return dp[n];
 }
+
+int fibBottomUp(int n)
+{
+    //generally bottomup in DP is iterarive approach
+    int *dp=new int[n+1];
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=dp[i-2]+dp[i-1];
+    }
+    return dp[n];
+    //it can be done by this way also
+  /*  int a,b;
+    a=0;
+    b=1;
+    if(n<2)
+        return n;
+    for(int i=2;i<=n;i++)
+    {
+        int c;
+        c=b;
+        b=a+b;
+        a=c;
+    }
+    return b; */
+}
 int main()
 {
 	int n;
@@ -27,7 +54,7 @@ int main()
 	long long int dp[1000];
 	for(int i=0;i<=n;i++)
 		dp[i]=-1;
-	cout<<topdowndp(n,dp);
+	cout<<fibBottomUp(n);
 	//cout<<fib(n);
 	return 0;
 }
