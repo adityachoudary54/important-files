5a2a58888319b84a1e815955df304d58b63323cf
Aditya choudhary <adityachoudhary54@gmail.com>
2018-07-25 00:10:43 +0530

    changed something

--- C++ important files/fibonacci number.cpp
+++ C++ important files/fibonacci number.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+long long  int fibi[200];
 int fib(int n)
 {
     if(n<=0)
@@ -7,11 +8,20 @@ int fib(int n)
     else if(n==1)
         return 1;
     else
-        return fib(n-1)+fib(n-2);
+        if(fibi[n-1]==-1)
+        	fibi[n-1]=fib(n-1);
+ 		if(fibi[n-2]==-1)
+        	fibi[n-2]=fib(n-2);
+        return fibi[n-1]+fibi[n-2];
+
 }
 int main()
 {
+	fibi[0]=0;
+	fibi[1]=1;
     int n;
     cin>>n;
+    for(int i=0;i<n;i++)
+    	fibi[i]=-1;
     cout<<fib(n);
 }
